6f066731a0632abd7a300fef8c60524e903a4e9e
Mark-Junge-Wang <48705802+Mark-Junge-Wang@users.noreply.github.com>
2019-04-04 11:50:30 +1100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 var app = express();
+var users = require('./models/users')
+var router = express.Router()
 
 app.get('/', function(req, res){
 
     res.send("homepage");
+})
+
+router.get('/users', function(req, res){
+
+    res.send(array);
 })
 
 app.listen(3000, function(){

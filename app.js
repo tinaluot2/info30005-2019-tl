0b3e9fe34ce7cc25022c926e7ec714824c7ea94d
Mark-Junge-Wang <48705802+Mark-Junge-Wang@users.noreply.github.com>
2019-04-09 13:00:47 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,56 @@
 const express = require('express');
 var app = express();
-var users = require('./models/users')
+const users = require('./models/users')
 var router = express.Router()
 
 app.get('/', function(req, res){
 
-    res.send("homepage");
+    res.send("Souper Noodle Lab");
 })
 
-router.get('/users', function(req, res){
+app.get('/users', function(req, res){
 
-    res.send(array);
+    res.send("users page");
+})
+
+app.get('/Ideas', function(req, res){
+
+    res.send("Reuse ideas page");
+})
+
+app.get('/marketplace', function(req, res){
+
+    res.send("marketplace page");
+})
+
+app.get('/search', function(req, res){
+
+    res.send("search page");
+})
+
+app.get('/about', function(req, res){
+
+    res.send("about page");
+})
+
+app.get('/recycling', function(req, res){
+
+    res.send("Recycling page");
+})
+
+app.get('/signup', function(req, res){
+
+    res.send("signup page");
+})
+
+app.get('/login', function(req, res){
+
+    res.send("login page");
+})
+
+app.get('/account', function(req, res){
+
+    res.send("account page");
 })
 
 app.listen(3000, function(){

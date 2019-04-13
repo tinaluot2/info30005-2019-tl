3ea749f609fbc1bad34d420e565f1d9ca3d4a5bd
giggleinu <45643658+giggleinu@users.noreply.github.com>
2019-04-13 18:40:27 +1000

    Create items.js

---
+++ models/items.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const itemSchema = mongoose.Schema({
+  _id: mongoose.Schema.ObjectId,
+  itemName: String,
+  itemDescription: String,
+  itemCategory: String,
+  itemHashtags: Array,
+  itemImage: String,
+});
+
+module.exports = mongoose.model('Item', userSchema);

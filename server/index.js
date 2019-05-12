aae258469819456c2773140bced4506415538cd6
Gigi <gigiscleung@gmail.com>
2019-05-13 01:54:09 +1000

    fix index

--- server/index.js
+++ server/index.js
@@ -51,13 +51,13 @@ app.use((error, req, res, next) => {
   });
 });
 
-//Handle production
-if(process.env.NODE_ENV ==='production') {
-  //static folder
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+  // Static folder
   app.use(express.static(__dirname + '/public/'));
 
-  //SPA
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + 'public/index.html'));
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
 module.exports = app;

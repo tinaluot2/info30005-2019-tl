16adce310677a0ad5f9e4e909590c88caf69cc76
giggleinu <45643658+giggleinu@users.noreply.github.com>
2019-05-21 00:31:43 +1000

    Revert "Merge pull request #32 from tinaluot2/Bookmarks2"
    This reverts commit 2a6877303c755e91ea05b5af55fd05e93e5563af.

--- client/src/pages/UserSavedLists.vue
+++ client/src/pages/UserSavedLists.vue
@@ -1,44 +1,13 @@
 <template>
-	<div class="background nav-spacing">
-		<div class="page-container">
-			<h1 class="page-title">Bookmarks</h1>
-			<div class="grid-container site-wide">
-				<item-card v-for="item in itemsList" v-bind:key="item._id" :item="item"></item-card>
-			</div>
-		</div>
-	</div>
+	<h1>UserSavedLists</h1>
 </template>
 
 <script>
-
-	import ItemCard from '../components/ItemCard/ItemCard'
-	import apiService from '@/apiService'
-
-	export default {
-		name: 'UserSavedLists',
-		components: {
-			'item-card': ItemCard
-		},
-		data() {
-			return {
-				itemsList: []
-			}
-		},
-		mounted() {
-			apiService.getItems().then((data) => {
-				this.itemsList = data
-				bus.$emit('loaded')
-			})
-		},
-		computed: {
-			currentUser() {
-				return this.$store.state.currentUser
-			}
-		}
-
-	}
+export default {
+  name: 'UserSavedLists'
+}
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 </style>
 

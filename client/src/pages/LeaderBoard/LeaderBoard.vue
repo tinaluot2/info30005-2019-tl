47969fff2095c5b6e94d080947fab8d012d51850
Marin <miida@student.unimelb.edu.au>
2019-05-24 15:14:38 +1000

    Styling Leaderboard

--- client/src/pages/LeaderBoard/LeaderBoard.vue
+++ client/src/pages/LeaderBoard/LeaderBoard.vue
@@ -1,38 +1,27 @@
 <template>
 	<div class="background nav-spacing">
 		<div class="container">
-
 			<div class="white-card-bg">
 				<h1 class="page-header">Leaderboard</h1>
 				<h2 class="form-label"><i class="fab fa-pagelines"></i>Users with the most number of projects</h2>
-
-				<ol class="user-box">
-					<li v-for="user in usersList.slice(0, 5)" v-bind:key="user._id">
-						<div class="user-info">
-							<div class="contents">
-								<router-link :to="'/user/' + user.username">
-									<figure class="image is-48x48 is-rounded "><img class=" is-rounded" src="https://i2.wp.com/fosteredmedia.com/wp-content/uploads/2018/07/female-placeholder.jpg?fit=1024%2C1024&ssl=" alt="Smol_Dog"></figure>
-									<label >{{user.username}}</label>
-								</router-link>
-							</div>
-							<label class="contents" >{{userPosts(itemsList,user.username).length}} projects</label>
-						</div>
-					</li>
-				</ol>
-
 				<table id="leaderboard">
 					<tr>
 						<th>Rank</th>
 						<th>Reformer</th>
 						<th># of Projects</th>
 					</tr>
-					<tr v-for="(user, index) in usersList.slice(0, 10)" v-bind:key="index">
+
+					<tr v-for="(rank, index) in rankingList.slice(0, 10)" v-bind:key="index">
 						<td>{{index+1}}</td>
 						<td>
-							<router-link :to="'/user/' + user.username">{{user.username}}</router-link>
+							<router-link :to="'/user/' + rank.rankedUser">
+								<figure class="image is-48x48 is-rounded "><img class=" is-rounded" src="https://i2.wp.com/fosteredmedia.com/wp-content/uploads/2018/07/female-placeholder.jpg?fit=1024%2C1024&ssl=" alt="Smol_Dog"></figure>
+								{{rank.rankedUser}}
+							</router-link>
 						</td>
-						<td>{{userPosts(itemsList,user.username).length}}</td>
+						<td>{{rank.postCount}} projects</td>
 					</tr>
+
 				</table>
 
 			</div>
@@ -50,24 +39,34 @@
 					usersList: [],
 					itemsList: [],
 					loading: true,
-					rankList:[],
+					rankingList:{
+						rankedUser:'',
+						postCount:''
+					}
 				}
 			},
 			mounted() {
 				apiService.getUserProfile().then((data) => {
 					this.loading = false
 					this.usersList = data
-				})
+				});
 				apiService.getItemProfile().then((data) => {
 					this.loading = false
 					this.itemsList = data
-				})
+				});
+				this.userPosts();
+
 			},
 			methods:{
-				userPosts(itemsList, user){
-					return itemsList.filter(function(item){
-							return item.creatorName === user
-					})
+				userPosts(usersList, itemsList){
+					let i=0;
+
+					for(i=0; i < usersList; i++) {
+						this.rankingList[i].rankedUser=usersList[i].username
+						this.rankingList[i].postCount=itemsList.filter(function (item) {
+							return item.creatorName === usersList[i].username}).length
+
+					}
 				}
 			}
     }
